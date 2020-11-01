9d5fc2788436ebc77e0ccd400ed6d97438eac727
qmx <1639296181@qq.com>
2020-11-01 11:18:32 +0800

    table layout

--- app/src/main/java/com/example/oandr/MainActivity.java
+++ app/src/main/java/com/example/oandr/MainActivity.java
@@ -28,5 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, MainActivityTableLayout.class));
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/oandr/MainActivityTableLayout.java
@@ -0,0 +1,14 @@
+package com.example.oandr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityTableLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_table_layout);
+    }
+}
