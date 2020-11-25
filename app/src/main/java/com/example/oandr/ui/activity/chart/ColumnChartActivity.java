87ae6e334749e7d039c68fc0348b3a12021ab404
qmx <1639296181@qq.com>
2020-11-25 13:58:04 +0800

    column data

--- app/src/main/java/com/example/oandr/ui/activity/chart/ColumnChartActivity.java
+++ app/src/main/java/com/example/oandr/ui/activity/chart/ColumnChartActivity.java
@@ -6,7 +6,20 @@ import android.view.View;
 import com.example.oandr.R;
 import com.example.oandr.ui.activity.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ColumnChartView;
+
 public class ColumnChartActivity extends BaseActivity {
+
+    private ColumnChartView mColumnChartView;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_column_chart;
@@ -14,12 +27,25 @@ public class ColumnChartActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        mColumnChartView = (ColumnChartView) findViewById(R.id.cvc_main);
     }
 
     @Override
     public void initData() {
-
+        ArrayList<Column> columns = new ArrayList<>();
+        List<SubcolumnValue> values;
+        int numColumns = 8;
+        int numSubColumns = 1;
+        for (int i = 0; i< numColumns; i++){
+            values = new ArrayList<>();
+            for (int j = 0; j< numSubColumns; j++){
+                values.add(new SubcolumnValue((float)Math.random()*50, ChartUtils.pickColor()));
+            }
+            Column column = new Column(values);
+            columns.add(column);
+        }
+        ColumnChartData columnChartData = new ColumnChartData(columns);
+        mColumnChartView.setColumnChartData(columnChartData);
     }
 
     @Override
