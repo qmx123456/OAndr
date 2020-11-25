e860a8314a182b293c8292711011d7664d055f99
qmx <1639296181@qq.com>
2020-11-25 22:32:59 +0800

    pie data

--- app/src/main/java/com/example/oandr/ui/activity/chart/PieChartActivity.java
+++ app/src/main/java/com/example/oandr/ui/activity/chart/PieChartActivity.java
@@ -1,11 +1,23 @@
 package com.example.oandr.ui.activity.chart;
 
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.oandr.R;
 import com.example.oandr.ui.activity.base.BaseActivity;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.PieChartView;
+
 public class PieChartActivity extends BaseActivity {
+
+    private PieChartView mPieChartView;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_pie_chart;
@@ -13,12 +25,19 @@ public class PieChartActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        mPieChartView = (PieChartView) findViewById(R.id.pvc_main);
     }
 
     @Override
     public void initData() {
-
+        int numValues = 6;
+        ArrayList<SliceValue> sliceValues = new ArrayList<>();
+        for (int i=0;i<numValues;i++){
+            SliceValue sliceValue = new SliceValue((float) Math.random() * 30, ChartUtils.pickColor());
+            sliceValues.add(sliceValue);
+        }
+        PieChartData pieChartData = new PieChartData(sliceValues);
+        mPieChartView.setPieChartData(pieChartData);
     }
 
     @Override
