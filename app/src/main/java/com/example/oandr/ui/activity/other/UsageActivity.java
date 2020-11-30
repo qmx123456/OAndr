4ecdacfb0743f454a0e50558a42d0890eb96f5f8
qmx <1639296181@qq.com>
2020-11-30 16:28:35 +0800

    exchange pic

--- app/src/main/java/com/example/oandr/ui/activity/other/UsageActivity.java
+++ app/src/main/java/com/example/oandr/ui/activity/other/UsageActivity.java
@@ -1,11 +1,19 @@
 package com.example.oandr.ui.activity.other;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.view.View;
+
+import androidx.cardview.widget.CardView;
 
 import com.example.oandr.R;
 import com.example.oandr.ui.activity.base.BaseActivity;
+import com.example.oandr.ui.activity.usage.ExchangeActivity;
 
 public class UsageActivity extends BaseActivity {
+
+    private CardView exchangeCardView;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_use_scene;
@@ -13,7 +21,7 @@ public class UsageActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        exchangeCardView = (CardView) findViewById(R.id.cv_usage_exchange);
     }
 
     @Override
@@ -23,11 +31,16 @@ public class UsageActivity extends BaseActivity {
 
     @Override
     public void initListener() {
-
+        exchangeCardView.setOnClickListener(this);
     }
 
     @Override
     public void processClick(View v) {
-
+        switch (v.getId()){
+            case R.id.cv_usage_exchange:
+                Intent i1 = new Intent(this, ExchangeActivity.class);
+                startActivity(i1);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/oandr/ui/activity/usage/ExchangeActivity.java
@@ -0,0 +1,33 @@
+package com.example.oandr.ui.activity.usage;
+
+import android.view.View;
+
+import com.example.oandr.R;
+import com.example.oandr.ui.activity.base.BaseActivity;
+
+public class ExchangeActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_use_exchange;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
